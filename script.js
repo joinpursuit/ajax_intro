c6f6ad32b7b02ccdf8b5cce5e20d3a3b24add216
your name here <jonellebain@pursuit.org>
2018-11-15 17:53:52 -0500

    pushed

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () =>{
+  let button = document.querySelector(".button")
+  button.addEventListener("click", (event) =>{
+
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+
+getRandomImage
+
+// })
+
+xhr.open("get", "https://pokeapi.co/api/v2/pokemon/")
+xhr.send()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// xhr.open("get", "https://dog.ceo/api/breeds/list/all")
+// xhr.send()
+//
+// // })
