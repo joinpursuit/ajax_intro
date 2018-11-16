2e995ca5a53fc7891f3d8f5f221a7659fb587874
Michell Tejada <michelltejada@pursuit.com>
2018-11-16 00:24:08 -0500

    Ajax Exercise

--- ajax.js
+++ ajax.js
@@ -1,40 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
 //
+let image = document.createElement("img")
+
   let dog = new XMLHttpRequest()
   dog.onreadystatechange = function () {
     if(dog.readyState === 4) {
       if(dog.status === 200) {
         let dogList = JSON.parse(dog.response)
-        console.log(dog);
+        // console.log(dog);
         // console.log("hi: ", dogList)
-        let displayPhotos = dogList.message
-        let pictures = document.querySelector(".pictures")
-        let dogPic = dogList.message
-        // for(let i = 0; i < displayPhotos.length; i++) {
-            let image = document.createElement("img")
-            image.innerHTML = displayPhotos
-            pictures.appendChild(image)
-        // }
+        image["src"] = dogList.message
+        // let pictures = document.querySelector(".pictures")
+        // let dogPic = dogList.message
+        // for(let i = 0; i < displayPhotos.length; i++)
+        document.body.appendChild(image)
       }
     }
   }
 
+
   function getRandomImage() {
-
-      dog.open("get", "https://dog.ceo/api/breeds/image/random", true)
-      dog.send()
-    }
-
+    dog.open("get", "https://dog.ceo/api/breeds/image/random", true)
+    dog.send()
+  }
 
   let button = document.getElementById('button')
   button.addEventListener("click", () => {
 
     console.log("clicked")
-    let result = getRandomImage()
-    let image = document.createElement("img")
-    image.src = dogList.message
 
+    getRandomImage();
 
+  });
 
-  })
 });
