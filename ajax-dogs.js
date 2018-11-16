1d4956b1abf99f2acc9becbbb90b764c08217f0d
Nielene Thomas <nielenethomas@pursuit.org>
2018-11-16 18:39:12 -0500

    3rd commit

--- ajax-dogs.js
+++ ajax-dogs.js
@@ -1,20 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
   let button = document.querySelector("button");
-  button.addEventListener("click", (event) => {
-    console.log("Clicked!");
+  let xhr = new XMLHttpRequest();
+  let body = document.querySelector("body");
+  let image = document.createElement("img");
+  let dogObj;
+  console.log(image);
+  console.log(image.src);
+  // let imgsrc = document.createElement("image.src")
+  // imgsrc = dogObj.message;
+  // console.log(imgsrc);
+  // image.appendChild(imgsrc);
 
 
-    function getRandomImage (xhr) {
-      // console.log("hello");
-      xhr.open("get", "https://dog.ceo/api/breeds/image/random", true)
-      xhr.send()
-      console.log("hello");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        dogObj = JSON.parse(xhr.response);
+        body.appendChild(img)
+      }
     }
+    console.log(dogObj);
+  }
 
+
+  function getRandomImage () { // console.log("hello, You");
+    xhr.open("get", "https://dog.ceo/api/breeds/image/random", true)
+    xhr.send()
+
+    // console.log("hello");
+  }
+
+
+  button.addEventListener("click", (event) => {
+    // console.log("Clicked!");
     getRandomImage()
-
-
-
-
+    // console.log(getRandomImage());
   })
+
+
+
+
+
 })
