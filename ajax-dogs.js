ef771d6f7cdd4cfbc336c398e4c3defc48869de3
Nielene Thomas <nielenethomas@pursuit.org>
2018-11-18 23:35:45 -0500

    5th commit

--- ajax-dogs.js
+++ ajax-dogs.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let xhr = new XMLHttpRequest();
   let body = document.querySelector("body");
   let image = document.createElement("img");
+  // let theImage= document.querySelector(image);
   let dogObj;
   // let dogObj("message");
   image.setAttribute("src", "https://images.dog.ceo/breeds/corgi-cardigan/n02113186_13256.jpg");
@@ -20,11 +21,18 @@ document.addEventListener("DOMContentLoaded", () => {
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
         dogObj = JSON.parse(xhr.response);
-        body.appendChild(image)
+        // body.appendChild(image);
+
+        image.src = dogObj.message;
+        if (xhr.status === 0) {
+          body.appendChild(image);
+        } else if (xhr.status > 0) {
+          document.querySelector(image).replaceChild(image.src, image)
+        }
       }
     }
-    console.log("dogObj", dogObj);
-    console.log("dogObj.message", dogObj.message);
+    // console.log("dogObj", dogObj);
+    // console.log("dogObj.message", dogObj.message);
   }
 
 
@@ -35,21 +43,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
   button.addEventListener("click", (event) => {
-    if (dogObj.message) {
+
       getRandomImage()
       // console.log("variable", xhr.onreadystatechange);
       // // console.log(dogObj.message);
-      document.querySelector("src").replaceChild(src, src)
+      // document.querySelector("src").replaceChild(src, src)
       // image.parentNode.replaceChilde(image, dogObj.message)
       // image.src = dogObj.message
-
-    }
-
-
   })
-
-
-
-
 
 })
